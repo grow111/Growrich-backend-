f385804f62b0075d07c7a5110bac0d0d205dff52
grow111 <ritaora2045@gmail.com>
2025-07-04 13:47:26 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
+const investmentSchema = new mongoose.Schema({
+  amount: Number,
+  investedAt: { type: Date, default: Date.now },
+  payoutAt: Date,
+  isWithdrawn: { type: Boolean, default: false },
+});
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   investmentAmount: { type: Number, default: 0 },
+  investments: [investmentSchema],
   referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  investments: [{
-    amount: Number,
-    investedAt: Date,
-    payoutAt: Date,
-    isWithdrawn: { type: Boolean, default: false }
-  }],
-  receipt: {
-    filename: String,
-    originalname: String,
-    uploadedAt: Date
-  }
+  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  paymentReceipt: { type: String, default: '' },
+  isPaid: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
